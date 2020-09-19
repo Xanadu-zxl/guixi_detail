9cd9c66c723b6797169bbedc12ce512a2d48c304
xanadu-zxl <xanadu.zxl@gamil.com>
2020-09-19 20:44:45 +0800

    feat：4040页面

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>404</div>
+  <div class="header">
+    <img src="../assets/img/404.png" alt="" class="img" />
+    <p>对不起，您请求的页面不存在、或已被删除、或暂时不可用</p>
+    <p class="link" @click="back">返回上一页</p>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {}
   },
+  mounted() {
+    document.title = '404网页找不到～'
+  },
+  methods: {
+    back() {
+      this.$router.go(-1)
+    },
+  },
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  margin-top: 50px;
+
+  .img {
+    width: 80%;
+    height: 100%;
+  }
+
+  p {
+    padding: 20px;
+  }
+
+  .link {
+    color: rgb(102, 166, 238);
+  }
+}
+</style>
